1db60bdac1bd63bc6acaeb52404af835e2fc558f
Karen Seunsom <karenseuns@gmail.com>
2025-03-18 19:09:58 -0500

    added buttons to patient dashboard and created new component files

---
+++ frontend/src/components/PatientDashboard.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+
+export default function PatientDashboard(){
+
+
+    return(
+    <div className="flex justify-center flex-row flex-nowrap gap-2 text-sm font-mono">
+        
+        {/* will need to add links to all buttons later */}
+        <Link as={Link} className="border rounded-l shadow-lg p-3 h-full text-center min-w-3xs"><h4>Schedule an Appointment</h4> </Link> 
+        <Link as={Link} className="border rounded-l shadow-lg p-3 h-full text-center min-w-3xs"><h4>Visits</h4> </Link> 
+        <Link as={Link} className="border rounded-l shadow-lg p-3 h-full text-center min-w-3xs"><h4>Something else</h4> </Link> 
+        <Link as={Link} className="border rounded-l shadow-lg p-3 h-full text-center min-w-3xs"><h4>Something else</h4> </Link> 
+        <Link as={Link} className="border rounded-l shadow-lg p-3 h-full text-center min-w-3xs"><h4>Billing</h4> </Link> 
+
+
+    </div>
+    )
+}
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,21 @@
+import { useContext} from 'react';
+import {UserContext} from '../context/Usercontext';
+import PatientDashboard from '../components/PatientDashboard.jsx';
+import AdminDashboard from '../components/AdminDashboard.jsx';
+import DoctorDashboard from '../components/DoctorDashboard.jsx';
+
+
 export default function Dashboard(){
+    const {role, setRole} = useContext(UserContext)
+
+
     return(
-        <div className = "home-page">
-            <h1>Dashboard</h1>
-            <p>This will be the dashboard depending on the user</p>
+        <div className = "bg-[#F8F9FA] m-4 p-8 shadow rounded-lg w-full mt-8">
+            <h1 className="font-serif text-3xl text-center my-4">Welcome, {role}!</h1>
+            {role === 'patient' && <PatientDashboard />}
+            {role === 'admin' && <AdminDashboard />}
+            {role === 'doctor' && <DoctorDashboard />}
+
         </div>
     )
 
