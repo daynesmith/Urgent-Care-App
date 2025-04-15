dd32b50783d512fea8adab0f5cd5c41d56169c51
billykngo <10876150+billykngo@users.noreply.github.com>
2025-04-15 06:33:07 -0500

    delete all associated aplications files

--- backend/routes/Applications.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const {updateApplicationStatus} = require('../controllers/applicationsController')
-
-router.post("/updateApplicationStatus", updateApplicationStatus);
-
-module.exports = router;
